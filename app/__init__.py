7f6e8c5be67d19254f9845b83a39167325fc2f31
nicolesouydalay <nicolesouydalay@gmail.com>
2021-06-22 17:46:15 +0000

    Set up database and website as a serivce.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask, render_template, send_from_directory
 from dotenv import load_dotenv
+from . import db
 
 load_dotenv()
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('index.html', title="Nicole Souydalay | ", url=os.getenv("URL"), name="NICOLE")
 
+@app.route('/health', methods = ['GET'])
+def health():
+    return 'Works'
